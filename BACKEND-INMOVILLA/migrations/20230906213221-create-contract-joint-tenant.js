a1fa4be86564c84e365ae98283e57affbf953754
David Leguizamon <davidsantiagolguerra@gmail.com>
2023-09-19 14:35:54 -0500

    feat: relationship update

--- BACKEND-INMOVILLA/migrations/20230906213221-create-contract-joint-tenant.js
+++ BACKEND-INMOVILLA/migrations/20230906213221-create-contract-joint-tenant.js
@@ -22,7 +22,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         references:{
           model:{
-            tableName:"jointTenants",
+            tableName:"jointtenants",
             key:"id"
           }
         }
