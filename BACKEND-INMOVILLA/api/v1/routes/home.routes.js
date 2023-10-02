b2bc491e4eab5b9cd84e6f2e476fb519f06e0d8c
Felipebastidas <juanfelipebastidas1105@gmail.com>
2023-10-02 12:08:24 -0500

    feat:added the home controller and service

---
+++ BACKEND-INMOVILLA/api/v1/routes/home.routes.js
@@ -0,0 +1,14 @@
+const {Router} = require('express')
+const homeController = require('../../../controllers/homeController')
+
+
+const router = Router()
+
+
+router.get('/', homeController.getAll)
+router.get('/:id', homeController.get)
+router.post('/',homeController.create)
+router.put('/:id',homeController.update)
+router.delete('/:id',homeController.destroy)
+
+module.exports = router;
---
+++ BACKEND-INMOVILLA/controllers/homeController.js
@@ -0,0 +1,62 @@
+// controllers/administradorController.js
+const HomeService = require('../services/homeService');
+
+
+  const getAll = async (req, res) => {
+    try {
+      const resul  = await HomeService.getAll();
+      res.status(200).send({status:"OK", data: resul});
+    } catch (error) {
+      res.status(500).json({ error: error.message }); 
+    }
+  }
+
+  const get= async (req, res) => {
+    let  id = req.params.adminId;
+    try {
+      const administrador = await HomeService.get(id);
+      res.status(200).send({status:"OK", data:administrador})
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  const create = async (req, res) =>  {
+    const data = req.body;
+    try {
+      const nuevoAdministrador = await HomeService.create(data.Codigo_Vivienda, data.Ubicacion, data.Cant_Cuartos, data.Caracteristicas_Extra, data.Tiene_Servicios_Incluidos, data.Tipo_Objeto, data.Area_Inmueble, data.Precio, data.Descripcion, data.Tiene_Garaje_Moto,data.Tipo_Vivienda,data.Tiene_Garaje_Carro,data.Tiene_Patio,data.Cant_Baños,data.Precio_Venta,data.FKId_Agente,data.typeHousing,data.typeTarget);
+      res.status(201).json(nuevoAdministrador);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  const update= async (req, res) =>  {
+    let id  = req.params.id;
+    const data = req.body;
+    try {
+      const administradorActualizado = await HomeService.update(id,data.Codigo_Vivienda, data.Ubicacion, data.Cant_Cuartos, data.Caracteristicas_Extra, data.Tiene_Servicios_Incluidos, data.Tipo_Objeto, data.Area_Inmueble, data.Precio, data.Descripcion, data.Tiene_Garaje_Moto,data.Tipo_Vivienda,data.Tiene_Garaje_Carro,data.Tiene_Patio,data.Cant_Baños,data.Precio_Venta);
+      res.status(200).send({status:"OK", data:administradorActualizado})
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  const destroy = async (req, res) =>  {
+    let id = req.params.id;
+    try {
+      const deletedAdmin = await HomeService.destroy(id);
+      res.status(204).send({status:"OK",data:deletedAdmin});
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+module.exports = {
+  getAll,
+  get,
+  create,
+  update,
+  destroy
+}
+
